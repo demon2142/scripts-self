c127ef6c0ad165120da1123c9ef509c595ff1d83
dkovalev <d.kovalev@napoleonit.ru>
2024-05-07 12:15:29 +0500

    add some files

---
+++ install-first.sh
@@ -0,0 +1,10 @@
+mkdir -p ~/bin
+# make link actual python binaries
+ln -s $(which python3) python
+ln -s $(which pip3) pip
+
+##########################
+# or sudo vim /etc/bash.bashrc
+# alias python=python3
+# alias pip=pip3
+#################
