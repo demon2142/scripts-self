d25207ab6a8100fe146e88f477edf32aff7107f2
dkovalev <d.kovalev@napoleonit.ru>
2024-06-06 10:00:01 +0500

    Auto-commit on 06-06-2024 10:00:01

--- init-new-vm.sh
+++ init-new-vm.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
-USERNAME="support"
-SSH_KEY="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC+qxUnsUBurXv43Kn2yN2cxPdg4f0rzP4otnzYVG027J8XHh8hMi6vnoxg0CHeZ0y5m9GMaC3bWmC
-gNnVE7GtV+GlyKcAJxv2IKSrFE7ZsxgjlRLFCKIa5qNWq8e/ag2xCssGiqcNvMB/P0HlyK+RuAowaKONUU+AL0vhzhnljjunN+TyBhaPQ4ARBYBi1D59DVRV8YUd
-wH2RzWd0VpENZXxVZmauXvJW4B70lOCNkFDcZ0l9RhnDYUlfd2aDbaJxLTj2vTCA9cLLDepYLf334yYigzbGxNYqAqq7usDm5BwAOiv11lubRucePZq/kdbhYDA9
-dLvOw5Kh1C+uJ2uiB denis@desktop"
+USERNAME="odmin"
+SSH_KEY="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCzYvZ6rEGXUC3kLhowDshEyfDVCez1ymXB2qTbAK94dkGeQpShDroIkD8xGdxwOZuUcZL43VYgRwPPtxPBECFD
+XIb02W/NklszqM0rfz57EJ44+RXGnUORq9KHEhf4OM4rWb7Er14rLExTkt/QdCnxkECh6EKJ8zj0okGwNdViJ0wf6JcCiF2rWhEfgb43Vc9ZU3q8JqcP3JfGVbVA
+axAqFkGRUj7KVrqyCe6gf/6ZGdWb4J3ZKsoD2cPwZt93BgDRVZOePjJBs8W8aiuQSI2syWkIS3AIeDSPZBC4bBY+vIuxdeHvfk12Qe1NEGTjAysh3OvtBdJAoY+b
+G3wWjvfl dkovalev@dkovalev-ubuntu"
+PASSWORD_HASH='$6$9JyebNbAZXx5fCTg$a0B90Q454GXTYmNR0eunQ54KooXmOxPyGiyo5keQTX0rBifoxEyAf7QSYDDGweGPTxB7eDPt5wNzULeMV5uBZ.'
 
 function create_user(){
-    useradd -m -s /bin/bash -G sudo ${USERNAME}
+    useradd -m -s /bin/bash -G sudo -p "$PASSWORD_HASH" ${USERNAME}
     echo "${USERNAME} ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/${USERNAME}
     }
 
