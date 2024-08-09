ee8ea8081bc0537581115612198e57b08ef238b4
dkovalev <d.kovalev@napoleonit.ru>
2024-08-09 14:00:01 +0500

    Auto-commit on 08-09-2024 14:00:01

--- other-scripts/cloudflare_zones_update_prometheus_config.py
+++ other-scripts/cloudflare_zones_update_prometheus_config.py
@@ -1,13 +1,16 @@
 import requests
 import yaml
 import time
+import os
 
 # Настройки
 API_TOKEN = '5jYrDJbwtaYaSMTX1RCbd5u0Ew7x-FON4i9UIXRa'  # Замените на ваш API токен
 API_EMAIL = 'd.kovalev@napoleonit.ru'  # Замените на ваш email
-PROMETHEUS_FILE = 'targets.yml'  # Путь к файлу конфигурации Prometheus
+PROMETHEUS_FILE = 'other_domains.yml'  # Путь к файлу конфигурации Prometheus
 RATE_LIMIT = 200  # Лимит запросов в минуту
 DELAY = 60 / RATE_LIMIT  # Задержка между запросами (в секундах)
+LOG_FILE = '/var/log/prometheus/prometheus_targets_update.log'  # Путь к файлу логов
+TEMP_FILE = '/tmp/prometheus_targets_temp.yml'  # Определение переменных
 
 # Заголовки для авторизации
 headers = {
@@ -31,15 +34,19 @@ def get_a_records(zone_id):
     response.raise_for_status()
     return response.json().get('result', [])
 
-# Функция для очистки и формата доменов
+# Функция для очистки и форматирования доменов
 def clean_domain(domain_name):
-    # Убираем префикс 'www.' и все точки в начале
+    # Убираем префикс 'www.'
     if domain_name.startswith("www."):
         domain_name = domain_name[4:]
-    
-    # Удаляем начальные точки
-    domain_name = domain_name.lstrip('.')
 
+    # Удаляем начальные точки и звездочки
+    domain_name = domain_name.lstrip('.').lstrip('*').lstrip('.')
+    
+    # Удаляем '.*'
+    if domain_name.startswith("*"):
+        domain_name = domain_name[1:]
+    
     return domain_name
 
 # Функция для обновления Prometheus конфигурации
@@ -69,16 +76,32 @@ def generate_prometheus_config():
         "targets": list(https_targets)
     }]
 
-    with open(PROMETHEUS_FILE, 'w') as f:
+    # Запись в временный файл
+    with open(TEMP_FILE, 'w') as f:
         yaml.dump(prometheus_config, f, default_flow_style=False)
     
-    print(f"Конфигурация Prometheus обновлена в {PROMETHEUS_FILE}")
+    # Проверка временного файла
+    try:
+        with open(TEMP_FILE, 'r') as f:
+            yaml.safe_load(f)
+    except yaml.YAMLError as e:
+        log(f"Ошибка YAML при проверке временного файла: {e}")
+        return
+    
+    # Замена основного файла конфигурации
+    os.rename(TEMP_FILE, PROMETHEUS_FILE)
+    log(f"Конфигурация Prometheus обновлена в {PROMETHEUS_FILE}")
+
+# Функция для записи логов
+def log(message):
+    with open(LOG_FILE, 'a') as f:
+        f.write(f"{time.strftime('%Y-%m-%d %H:%M:%S')} - {message}\n")
 
 # Основной скрипт
 def main():
-    print("Обновление данных и конфигурации Prometheus")
+    log("Обновление данных и конфигурации Prometheus начато")
     generate_prometheus_config()
-    print("Завершено.")
+    log("Обновление завершено")
 
 if __name__ == "__main__":
     main()
